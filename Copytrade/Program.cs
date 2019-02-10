f2d07673d9991351c2002f35352778a1ba086512
olegsokolansky <oleg.sokolansky@gmail.com>
2019-02-10 11:21:01 +0200

    * .gitignore: make udp work
    * 1.csproj:
    * Program.cs:
    * Copytrade.sln:
    * Program.cs:
    * Program.cs:
    * Receiver.csproj:
    * Copytrade.csproj:
    * AssemblyInfo.cs:

---
+++ Copytrade/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace Copytrade
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            IPEndPoint Sergiicalledtogod = new IPEndPoint(IPAddress.Any, 40000);
+            Socket WinSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            WinSocket.Bind(Sergiicalledtogod);
+
+            Console.Write("Waiting for client");
+            IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
+            EndPoint Remote = (EndPoint)(sender);
+            byte[] data = new byte[1024];
+            int recv = WinSocket.ReceiveFrom(data, ref Remote);
+            Console.WriteLine("Message received from {0}:", Remote.ToString());
+            Console.WriteLine(Encoding.ASCII.GetString(data, 0, recv));
+        }
+    }
+}
---
+++ Receiver/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Receiver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var port = 40000;
+            IPEndPoint anyoneThroughPort = new IPEndPoint(IPAddress.Loopback, port);
+            var client = new UdpClient(anyoneThroughPort);
+            while (true)
+            {
+                var data = client.Receive(ref anyoneThroughPort);
+                Console.WriteLine(Encoding.ASCII.GetString(data));
+            }
+        }
+    }
+}
