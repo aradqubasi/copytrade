cf2ff86061a12f5cbab61d42813e56a3acf61b85
olegs <olegs@DESKTOP-58RI2LJ>
2019-02-19 21:07:52 +0200

    parse signal

--- Receiver/Program.cs
+++ Receiver/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
@@ -15,7 +17,47 @@ namespace Receiver
             while (true)
             {
                 var data = client.Receive(ref anyoneThroughPort);
-                Console.WriteLine(Encoding.ASCII.GetString(data));
+                var signal = new TradeSignal();
+                var haveTradeType = false;
+                var haveAmount = false;
+                using (var reader = new JsonTextReader(new StreamReader(new MemoryStream(data))))
+                {
+                    while (reader.Read())
+                    {
+                        if (reader.TokenType == JsonToken.String)
+                        {
+                            if ((string)reader.Value == "buy")
+                            {
+                                signal.buyOrSell = TradeType.Buy;
+                                haveTradeType = true;
+                            }
+                            else if ((string)reader.Value == "sell")
+                            {
+                                signal.buyOrSell = TradeType.Sell;
+                                haveTradeType = true;
+                            }
+                            else
+                            {
+                                Console.WriteLine($"Unexpected buy_or_sell value {reader.Value}");
+                                break;
+                            }
+                        }
+                        else if (reader.TokenType == JsonToken.Integer)
+                        {
+                            signal.amount = (long)reader.Value;
+                            haveAmount = true;
+                        }
+                    }
+                }
+                if (haveTradeType && haveAmount)
+                {
+                    Console.WriteLine($"Incoming message was parsed successfully {signal.buyOrSell} {signal.amount}");
+                }
+                else
+                {
+                    Console.WriteLine($"Incoming message {Encoding.ASCII.GetString(data)}");
+                }
+                //Console.WriteLine(Encoding.ASCII.GetString(data));
             }
         }
     }
---
+++ Receiver/TradeSignal.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Receiver
+{
+    class TradeSignal
+    {
+        public TradeType buyOrSell;
+        public long amount;
+    }
+}
